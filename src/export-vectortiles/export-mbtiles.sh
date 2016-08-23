5f2bcbc94a37a8257aed656cfa64ed934631670d
Axel Utech <axel.utech@gmail.com>
2016-08-23 17:51:54 +0200

    add a sample seamarks layer

--- src/export-vectortiles/export-mbtiles.sh
+++ src/export-vectortiles/export-mbtiles.sh
@@ -6,12 +6,12 @@ set -o nounset
 readonly PROJECT_DIR="/tm2source"
 readonly EXPORT_DIR="/export"
 readonly RENDER_SCHEME=${RENDER_SCHEME:-pyramid}
-readonly MIN_ZOOM=${MIN_ZOOM:-7}
-readonly MAX_ZOOM=${MAX_ZOOM:-14}
-readonly BBOX=${BBOX:-"5.8559113 45.717995 10.5922941 47.9084648"}
+readonly MIN_ZOOM=${MIN_ZOOM:-0}
+readonly MAX_ZOOM=${MAX_ZOOM:-20}
+readonly BBOX=${BBOX:-"11.96 54.042 12.24 54.23"}
 
 function export_local_mbtiles() {
-    local mbtiles_name="natural_earth.mbtiles"
+    local mbtiles_name="oseam.mbtiles"
 
     exec tl copy \
         -s pyramid \
